759470098bd2530815d10b83d08913b7dc9ed618
turcuciprian <turcuciprian1@gmail.com>
2015-09-26 17:55:20 +0300

    first commit

---
+++ themeDeploy.php
@@ -0,0 +1,11 @@
+<?php
+/*
+  Plugin Name: Theme Deploy
+  Plugin URI: http://turcuciprian.com
+  Description: Easily commit, push and pull your theme changes to github or bitbucket
+  Version: 0.0.1
+  Author: turcuciprian
+  Author URI: http://turcuciprian.com
+  License: GPLv2 or later
+  Text Domain: usefulPlugin
+ */
